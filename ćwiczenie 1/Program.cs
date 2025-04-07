6afdd13ed63211f6a0446c8588270556d03ba49b
Oliwia Bryks <oliwiabryks04@gmail.com>
2025-04-07 15:28:30 +0200

    koniex

---
+++ ćwiczenie 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ćwiczenie_1
+{
+    internal class Program
+    {
+            //Napisz funkcję o nazwie suma wyznaczającą sumę jej dwóch argumentów będących liczbami
+            //całkowitymi.Napisz program wykorzystujący funkcję suma.
+            
+            static int suma(int a, int b)
+            {
+                return a + b;
+            }
+
+            static void Main(string[] args)
+            {
+            int a, b;
+            Console.WriteLine("Podaj pierwsza liczbe a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Suma {0} i {1} wynosi: {2}", a, b, suma(a, b));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ ćwiczenie 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ćwiczenie_2
+{
+    internal class Program
+    {
+        // Napisz funkcję o nazwie max, która wyznacza maksimum z dwóch liczb rzeczywistych
+        //zadanych jako argumenty tej funkcji.Napisz program wykorzystujący funkcję max.
+
+        static double max(double a, double b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.WriteLine("Podaj pierwsza liczbe a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Maksimum {0} i {1} wynosi: {2}", a, b, max(a, b));
+            Console.ReadKey(true);
+        }
+    }
+}
