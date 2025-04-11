dac5bccf891f461538e18cd3857c47f4ddf1883a
Oliwia Bryks <oliwiabryks04@gmail.com>
2025-04-11 12:02:05 +0200

    koniec

---
+++ ćwiczenie 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ćwiczenie_4
+{
+    internal class Program
+    {
+        static int ile_cyfr(int liczba)
+        {
+            int i = 0;
+            while (liczba/10 > 0)
+            {
+                liczba /= 10;
+                i++;   
+            }
+            return i + 1;
+        }
+        static void Main(string[] args)
+        {
+            //Napisz funkcję o nazwie ile_cyfr, która wyznacza liczbę cyfr dla zadanej liczby naturalnej jako
+            //argument tej funkcji.Napisz program wykorzystujący funkcję ile_cyfr.
+            int n;
+            Console.WriteLine("Podaj liczbę: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Liczba {0} ma {1} cyfr", n, ile_cyfr(n));
+        }
+    }
+}
---
+++ ćwiczenie 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ćwiczenie_5
+{
+    internal class Program
+    {
+        //Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+        //powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb.Przetestuj działanie
+        //utworzonej funkcji.
+
+        static int czy_pierwsza(int liczba)
+        {
+            int i;
+            for (i=2; i<=Math.Sqrt(liczba); i++)
+                 if (liczba%i==0)
+                    return 0;
+                return 1;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj liczbę");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}", czy_pierwsza(n));
+        }
+    }
+}
