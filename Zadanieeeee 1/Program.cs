8b592349f9b81fa558247980ee682390421d708d
Oliwia Bryks <oliwiabryks04@gmail.com>
2025-05-05 16:02:31 +0200

    koniec zajęć

---
+++ Zadanieeeee 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanieeeee_1
+{
+    internal class Program
+    {
+        //Zdefiniuj strukturę o nazwie Osoba posiadającą trzy pola: imie, nazwisko i rok_urodzenia.
+        //Zadeklaruj zmienną typu strukturalnego o nazwie o, wczytaj do niej wszystkie wartości pól od
+        //użytkownika i wypisz wszystkie pobrane wartości pól na ekranie.
+
+        struct Osoba
+        {
+            public string imie, nazwisko;
+            public int rok_urodzenia;
+
+        }
+        static void Main(string[] args)
+        {
+            Osoba o;
+            Console.WriteLine("Podaj imie: ");
+            o.imie = Console.ReadLine ();
+            Console.WriteLine("Podaj nazwisko: ");
+            o.nazwisko = Console.ReadLine(); 
+            Console.WriteLine("Podaj rok urodzenia: ");
+            o.rok_urodzenia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {1} urodził(a) się w {2} roku", o.imie, o.nazwisko, o.rok_urodzenia);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Zadanieeeee 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanieeeee_2
+{
+    internal class Program
+    {
+        //Zdefiniuj strukturę o nazwie Uczen zawierającą pola: imie, nazwisko i tablice ocen.
+        //Pobierz od użytkownika liczbę uczniów i zadeklaruj odpowiednią tablicę struktur
+        //umożliwiającą przechowanie informacji o imionach, nazwiskach i ocenach uczniów.
+        //Następnie pobierz dane od użytkownika i w sposób czytelny wypisz wszystko na ekranie.
+
+        struct Uczen
+        {
+            public string imie, nazwisko;
+            public byte[] oceny;
+        }
+        static void Main(string[] args)
+        {
+            Uczen[] uczniowie;
+            int ilu_uczniow, ile_ocen, i, j;
+            Console.WriteLine("Podaj ilu uczniów jest w klasie");
+            ilu_uczniow = Convert.ToInt32(Console.ReadLine());
+            uczniowie = new Uczen[ilu_uczniow];
+
+            for (i=0; i<ilu_uczniow; i++)
+            {
+                Console.WriteLine("Podaj imie ucznia {0}: ", i+1);
+                uczniowie[i].imie = Console.ReadLine();
+                Console.WriteLine("Podaj nazwisko ucznia {0}: ", i+1);
+                uczniowie[i].nazwisko = Console.ReadLine();
+                Console.WriteLine("Ile ocen ma {0} {1}", uczniowie[i].imie, uczniowie[i].nazwisko);
+                ile_ocen = Convert.ToInt32(Console.ReadLine());
+                uczniowie[i].oceny = new byte[ile_ocen];
+                for(j=0; j<ile_ocen; j++)
+                {
+                    Console.WriteLine("Podaj ocenę numer {0}", j + 1);
+                    uczniowie[i].oceny[j] = Convert.ToByte(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            for (i=0;i<ilu_uczniow;i++)
+            {
+                Console.WriteLine("{0}. {1} {2}", i + 1, uczniowie[i].imie, uczniowie[i].nazwisko);
+                foreach (byte o in uczniowie[i].oceny)
+                {
+                    Console.WriteLine("{0}, ", o);
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
